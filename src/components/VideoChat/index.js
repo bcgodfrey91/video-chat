420e6a2d6b661246001c5e6ec1415d16169590ec
Ben Godfrey <bcgodfrey91@gmail.com>
2019-10-16 21:23:22 -0500

    initial videochat set up

--- src/components/VideoChat/index.js
+++ src/components/VideoChat/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useCallback} from 'react';
 
 const VideoChat = () => {
   const [username, setUsername] = useState('');
   const [roomName, setRoomName] = useState('');
   const [token, setToken] = useState(null);
 
-  const handleUsernameChange = e => {
+  const handleUsernameChange = useCallback(e => {
     setUsername(e.target.value);
-  };
+  }, []);
 
-  const handleRoomNameChange = e => {
+  const handleRoomNameChange = useCallback(e => {
     setRoomName(e.target.value);
-  };
+  }, []);
+
+  const handleSubmit = useCallback(async e => {
+    e.preventDefault();
+    const res = await fetch('/video/token', {
+      method: 'POST',
+      body: JSON.stringify({
+        identity: username,
+        room: roomName
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const data = await res.json();
+    setToken(data.token);
+  }, [username, roomName]);
+
+  const handleLogout = useCallback(event => {
+    setToken(null);
+  }, []);
 
   return <div></div>
 };
